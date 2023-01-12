03414eb8239b6f565fcf584a78f06b7b8147f2ff
Hiago W. Petris <hiago.petris@gmail.com>
2023-01-12 08:42:56 -0300

    first commit

---
+++ bin/future.dart
@@ -0,0 +1,40 @@
+void main() {
+
+  // Future myFuture = Future((){
+  //   print('Going back to the Future');
+  //   return 21;
+  // }).then((value) => print('The value is $value'));
+
+  Future<int> myFutureFunc() async {
+    print('I have a function in the future');
+    await Future.delayed(Duration(seconds: 5));
+    throw Exception();
+    return 12;
+  }
+
+  // myFutureFunc()
+  //     .then((value) => print('My function value is: $value'))
+  //     .onError((error, stackTrace) => print('An Error ocurred'))
+  //     .whenComplete(() => print('The future is over'));
+
+  Future<int> myFutureErrorFunc(int a, int b) async{
+    try {
+      if (a>b) {
+        throw Exception();
+      }
+      print('I have a functional function');
+      await Future.delayed(Duration(seconds: 5));
+      return 42;
+    } catch(e) {
+      print('An error ocurred: $e');
+      return 42;
+    } finally {
+      print('The future is finally over');
+    }
+  }
+
+  myFutureErrorFunc(2, 1)
+    .then((value) => print("The value is $value"));
+
+  print('Done with main');
+}
